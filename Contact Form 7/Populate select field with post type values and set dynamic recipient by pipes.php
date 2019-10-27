f899dff49284d69cb6350319cbba62beabac18b2
Yuval Sabar <yuvalsabar@gmail.com>
2019-10-27 09:26:09 +0200

    pipes

---
+++ Contact Form 7/Populate select field with post type values and set dynamic recipient by pipes.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * Populate select field with titles and pipes in WPCF7
+ */
+function dynamic_select_field_values ( $scanned_tag, $replace ) {  
+    if ( $scanned_tag['name'] != 'department' )  
+        return $scanned_tag;
+
+    $posts = get_posts(
+        array ( 
+            'post_type'      => 'department',
+            'orderby'        => 'title',
+            'order'          => 'ASC',
+            'posts_per_page' => -1
+        )
+    );  
+  
+    if ( ! $posts )  
+        return $scanned_tag;
+
+    foreach ( $posts as $row ) {  
+        $scanned_tag['raw_values'][] = $row->post_title . '|' . get_field( 'email', $row->ID );
+    }
+
+    $pipes = new WPCF7_Pipes($scanned_tag['raw_values']);
+
+    $scanned_tag['values'] = $pipes->collect_befores();
+    $scanned_tag['pipes'] = $pipes;
+  
+    return $scanned_tag;  
+}  
+add_filter( 'wpcf7_form_tag', 'dynamic_select_field_values', 10, 2); 
+
+/**
+ * Dynamic Recipient with pipes
+ */
+function wpcf7_dynamic_recipient ($wpcf7) {
+    // The ID of the form
+    if ( $wpcf7->id() == 787 || $wpcf7->id() == 2520 ) {
+        $submission = WPCF7_Submission::get_instance();
+        $posted_data = $submission->get_posted_data();
+
+        $email_to = $posted_data['department'];
+        $mail = $wpcf7->prop( 'mail' );
+        $mail['recipient'] = $email_to;
+
+        $wpcf7->set_properties( array( 'mail' => $mail ) );
+    }
+}
+add_action( 'wpcf7_before_send_mail', 'wpcf7_dynamic_recipient' );
